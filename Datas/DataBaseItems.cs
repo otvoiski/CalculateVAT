80f9cf46f344694b9f9d423cf37421041195f34c
otvoi <otvoi@VOISKI>
2023-01-13 18:08:44 +0000

    Add project files.

---
+++ Datas/DataBaseItems.cs
@@ -0,0 +1,46 @@
+﻿using CalculateVAT.Models;
+
+namespace CalculateVAT.Data
+{
+    public class DataBaseItems
+    {
+        public static IList<VATDataModel> GetVatData()
+        {
+            return new List<VATDataModel>
+            {
+                new VATDataModel
+                {
+                    Name = "France",
+                    VAT = new double[]
+                    {
+                        5.5, 20, 10
+                    }
+                },
+                new VATDataModel
+                {
+                    Name = "United Kingdom",
+                    VAT = new double[]
+                    {
+                        5, 20
+                    }
+                },
+                new VATDataModel
+                {
+                    Name = "Portugal",
+                    VAT = new double[]
+                    {
+                        6, 13, 23
+                    }
+                },
+                new VATDataModel
+                {
+                    Name = "Spain",
+                    VAT = new double[]
+                    {
+                        21, 10
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ Models/VATDataModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CalculateVAT.Models
+{
+    public class VATDataModel
+    {
+        public double[] VAT { get; internal set; }
+        public string Name { get; internal set; }
+    }
+}
